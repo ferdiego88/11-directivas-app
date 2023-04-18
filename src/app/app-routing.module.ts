db1a8835fb380f7e900543ac7b69a4bea3e26dd3
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2023-04-18 18:53:29 -0500

    inicializar componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'productos',
+    loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+  },{
+    path:'**',redirectTo:'productos'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
